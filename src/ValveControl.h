a7dd89e5666548268e324e4091a789240d8767a1
ValentinSTM <112873891+ValentinSTM@users.noreply.github.com>
2023-11-19 07:06:30 +0100

    Update ValveControl.h

--- src/ValveControl.h
+++ src/ValveControl.h
@@ -2,8 +2,11 @@
 #ifndef VALVE_CONTROL_H
 #define VALVE_CONTROL_H
 
+void setupValveControl();
 void openValve();
 void closeValve();
 void manualValveSetting();
+void calibrateValve();
+void controlValveWithPID(double pidOutput);
 
 #endif
