6c0afe54ca13004654c6aa4615857be0ec9873a1
ValentinSTM <112873891+ValentinSTM@users.noreply.github.com>
2023-11-19 07:07:54 +0100

    Update ValveControl.cpp

--- src/ValveControl.cpp
+++ src/ValveControl.cpp
@@ -6,29 +6,46 @@
 const int pinValveOpen = D0;  // Example pin for opening the valve
 const int pinValveClose = D1; // Example pin for closing the valve
 
-void setupValveControl()
-{
+// Variable to store calibration data
+unsigned long fullOpenTime = 0;
+
+void setupValveControl() {
     pinMode(pinValveOpen, OUTPUT);
     pinMode(pinValveClose, OUTPUT);
 }
 
-void openValve()
-{
+void openValve() {
     digitalWrite(pinValveOpen, HIGH);
     digitalWrite(pinValveClose, LOW);
     delay(8500 * 15); // Delay for full 8.5mm stroke
     digitalWrite(pinValveOpen, LOW);
 }
 
-void closeValve()
-{
+void closeValve() {
     digitalWrite(pinValveClose, HIGH);
     digitalWrite(pinValveOpen, LOW);
     delay(8500 * 15); // Delay for full 8.5mm stroke
     digitalWrite(pinValveClose, LOW);
 }
 
-void manualValveSetting()
-{
+void manualValveSetting() {
     // Implement manual valve setting logic here
+}
+
+void calibrateValve() {
+    closeValve();
+    unsigned long startTime = millis();
+    openValve();
+    fullOpenTime = millis() - startTime;
+}
+
+void controlValveWithPID(double pidOutput) {
+    // Assuming pidOutput is between 0 (fully closed) and 100 (fully open)
+    unsigned long targetTime = (fullOpenTime * pidOutput) / 100;
+    
+    closeValve(); // Start from a known position
+    digitalWrite(pinValveOpen, HIGH);
+    digitalWrite(pinValveClose, LOW);
+    delay(targetTime); // Open the valve for a time proportional to pidOutput
+    digitalWrite(pinValveOpen, LOW); // Stop the valve
 }
