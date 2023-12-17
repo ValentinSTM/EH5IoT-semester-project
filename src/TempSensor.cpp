cc768ddf480d39a5ba47258fe332fecf18a1f617
ValentinSTM <112873891+ValentinSTM@users.noreply.github.com>
2023-12-17 19:00:52 +0100

    Moved functions relevant to TempSensor

--- src/TempSensor.cpp
+++ src/TempSensor.cpp
@@ -1,8 +1,9 @@
 // TempSensor.cpp
 #include "TempSensor.h"
-#include "DataProcessing.h"
+#include "RTDTable.h" // Include the RTD Table module header file
+#include <algorithm>  // Add this to include the standard algorithms library
 
-std::deque<float> resistanceSamples; // Queue of resistance samples for moving average
+std::deque<float> temperatureSamples; // Queue of resistance samples for moving average
 
 void setupTempSensor()
 {
@@ -25,7 +26,7 @@ float getTempValue()
     int sumOfSamples = 0;
     int noOfSamples = 10;
 
-    // Read the temperature from the PT500 sensor, and make an average of 10 samples
+    // Read the temperature from the PT500 sensor, and make an average of 10 samples to get a stable reading
     for (int i = 0; i < noOfSamples; i++)
     {
         sumOfSamples += readTempSensor();
@@ -33,21 +34,63 @@ float getTempValue()
     }
 
     int averageAdcValue = (int)(sumOfSamples / noOfSamples);
-    float resistance = calculateResistance(averageAdcValue); // Calculate the resistance from the ADC value
+    float resistance = calculateResistance(averageAdcValue);
     float temperature = interpolateTemperature(resistance);
-    Log.info("Resistance: %f, temperature %f", resistance, temperature);
+    Log.info("Current Resistance: %.2f, temperature %.2f", resistance, temperature);
 
-    // TODO fix this, decide where to put
-    // Moving average for spikes over longer time.
-    // static float sumOfSamplesMovingAverage = 0;
-    // sumOfSamplesMovingAverage += resistance; // Add the resistance to the sum of the samples for the moving average calculation
-    // // Serial.printlnf("Resistance: %f", resistance);    // Print the resistance to the serial monitor
-    // Serial.printlnf("Sum of samples: %f", sumOfSamplesMovingAverage); // Print the sum of the samples to the serial monitor
-    // resistanceSamples.push_back(resistance);             // Add the resistance to the resistance samples queue for the moving average calculation
+    // Moving average for spikes over longer time periods
+    // temperatureSamples.push_back(temperature); // add the resistance to the resistance samples queue for the moving average calculation
+    float averaged_temperature = calculateMovingAverage(temperature);
+    Log.info("Averaged temperature: %.2f", averaged_temperature);
 
-    // // Process the data to get the temperature
-    // float movingAverage = calculateMovingAverage(resistanceSamples, sumOfSamplesMovingAverage); // Calculate the moving average resistance value from the resistance samples queue and the sum of the samples for the moving average calculation
-    // float temperatureMovingAverage = interpolateTemperature(movingAverage);                     // Interpolate the temperature from the RTD table using the moving average resistance value and the RTD table data
+    return averaged_temperature;
+}
 
-    return temperature;
+// Function to calculate the moving average of resistance samples
+float calculateMovingAverage(float newTempSample)
+{
+    static float sumOfSamples = 0;
+
+    if (temperatureSamples.size() >= MOVING_AVERAGE_SAMPLE_SIZE) // If the number of samples is greater than the sample size
+    {
+        sumOfSamples -= temperatureSamples.front(); // Subtract the first element in the queue from the sum of the samples
+        temperatureSamples.pop_front();             // Remove the first element in the queue
+    }
+
+    temperatureSamples.push_back(newTempSample); // Add the new sample to the queue
+    sumOfSamples += newTempSample;               // Add the new sample to the sum
+
+    if (temperatureSamples.empty())
+    {
+        return 0; // If the queue is empty, average is 0
+    }
+    else
+    {
+        return sumOfSamples / temperatureSamples.size(); // Calculate the moving average of the temperature samples
+    }
+}
+
+// Function to interpolate temperature from resistance using the RTD table
+float interpolateTemperature(float measuredResistance)
+{
+    // Find the first element in the table that is greater than the measured resistance
+    auto it = std::upper_bound(rtdTable, rtdTable + rtdTableSize, measuredResistance,
+                               [](float a, const RtdValue &b)
+                               { return a < b.resistance; }); // a is measured_resistance, b is the RtdValue struct in the table
+
+    if (it == rtdTable + rtdTableSize) // If the measured resistance is greater than the last element in the table
+    {
+        return rtdTable[rtdTableSize - 1].temperature; // return the temperature of the last element in the table
+    }
+
+    if (it == rtdTable) // If the measured resistance is smaller than the first element in the table
+    {
+        return rtdTable[0].temperature; // return the temperature of the first element in the table
+    }
+
+    RtdValue upper = *it;     // upper is the first element in the table that is greater than the measured resistance
+    RtdValue lower = *(--it); // lower is the element before upper
+
+    return lower.temperature + (upper.temperature - lower.temperature) *
+                                   (measuredResistance - lower.resistance) / (upper.resistance - lower.resistance); // Linear interpolation between lower and upper element in the table
 }
--- src/TempSensor.h
+++ src/TempSensor.h
@@ -3,15 +3,21 @@
 #define TEMP_SENSOR_H
 
 #include <Particle.h>
+#include <deque> // Add this to include the double-ended queue library
 
-const int ANALOG_PIN = A0;           // PT500 connected to A0 pin on Photon2 (Analog pin) (ADC) (Analog to Digital Converter) (0-4095) (0-3.3V)
-const float REFERENCE_VOLTAGE = 3.3; // Reference voltage for Argon (3.3V) (3.3V/4095=0.00080586V)
-const float ADC_MAX_VALUE = 4095.0;  // 12-bit ADC resolution (4095) (2^12=4096) (0-4095) (0-3.3V)
-const float SERIES_RESISTOR = 596;   // Series resistor value
+const int ANALOG_PIN = A0;                   // PT500 connected to A0 pin on Photon2 (Analog pin) (ADC) (Analog to Digital Converter) (0-4095) (0-3.3V)
+const float REFERENCE_VOLTAGE = 3.3;         // Reference voltage for Argon (3.3V) (3.3V/4095=0.00080586V)
+const float ADC_MAX_VALUE = 4095.0;          // 12-bit ADC resolution (4095) (2^12=4096) (0-4095) (0-3.3V)
+const float SERIES_RESISTOR = 596;           // Series resistor value
+const size_t MOVING_AVERAGE_SAMPLE_SIZE = 5; // Number of samples for averaging (moving average)
+
+static std::deque<float> resistanceSamples; // Queue of resistance samples for moving average
 
 void setupTempSensor();
-int readTempSensor();                    // Function to read the analog value from the sensor
-float calculateResistance(int adcValue); // Function to convert the analog value to resistance
-float getTempValue();                    // Function to get the temperature from the PT500 sensor, after being calculated and averaged
+int readTempSensor();                                   // Function to read the analog value from the sensor
+float calculateResistance(int adcValue);                // Function to convert the analog value to resistance
+float getTempValue();                                   // Function to get the temperature from the PT500 sensor, after being calculated and averaged
+float calculateMovingAverage(float newTempSample);      // Function to calculate the moving average of resistance samples
+float interpolateTemperature(float measuredResistance); // Function to interpolate temperature from resistance using the RTD table
 
 #endif /* TEMP_SENSOR_H */
