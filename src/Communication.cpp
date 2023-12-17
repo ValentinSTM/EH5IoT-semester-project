06a7500a1e4e8d9b47db75fa820fd16b96fc6c69
ValentinSTM <112873891+ValentinSTM@users.noreply.github.com>
2023-12-17 18:59:57 +0100

    Renamed file

--- src/Communication.cpp
+++
@@ -1,7 +0,0 @@
-// Communication.cpp
-#include "Communication.h"
-
-void publishTemperature(float temperature) // Function to publish temperature data to Particle cloud (https://docs.particle.io/reference/device-os/firmware/photon/#particle-publish-)
-{
-    Particle.publish("temperature", String(temperature), PRIVATE); // Publish the temperature to the Particle Cloud
-}
